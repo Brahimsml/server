6389b32a97b88d128ccf6691e91525a26f8baa7c
Brahim Sml <72230485@students.liu.edu.lb>
2026-01-02 15:15:58 +0200

    update serverrrr

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const mysql = require("mysql");
+const mysql = require("mysql2");
 const path = require("path");
 const multer = require("multer");
 const jwt = require("jsonwebtoken");
